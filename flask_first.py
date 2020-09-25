592b9fcea2addb5d2052039b16210c2441ae0057
Alpaslan Kaplan <alp@perception.com.tr>
2020-09-25 13:22:10 +0300

    commit 2

--- flask_first.py
+++ flask_first.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def home():
     return ('<body><h1>Hello World</h1></body>')
 
-@app.route('/football', methods=['GET'])
-def football():
-    return render_template('football.html')
-
 if __name__ == '__main__':
     app.run(port = 5000, debug= True)
