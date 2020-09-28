50e50c8f960a9da5a31c17c7b26ecde860f6d5ba
Alpaslan Kaplan <alp@perception.com.tr>
2020-09-28 22:14:16 +0300

    added files

--- section2/flask_second.py
+++ section2/flask_second.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def home():
-    return ('Hello World')
+    return render_template('index.html')
+
+@app.route('/about', methods = ['GET'])
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
