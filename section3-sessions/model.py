887c1183ec4b35139121448eaa9301e01988cc60
alpaslank-73 <alp@perception.com.tr>
2020-11-03 02:42:58 +0300

    final commit

--- section3-sessions/model.py
+++ section3-sessions/model.py
@@ -1,32 +1,239 @@
 import pymysql 
 
-def check_pw(username):
+def getuid(email):
     connection = pymysql.connect( host = 'localhost', port = 3306,
     user = 'alp', passwd = '123456', db = 'todo' )
     cursor = connection.cursor()
-
-    #sql ='''SELECT password FROM users WHERE email ='{email}' ORDER BY uid DESC;'''.format(email = email)
-    sql = 'SELECT password FROM users WHERE email = email ;'
-    cursor.execute(sql)
-    password = cursor.fetchone()[0]
+    sql = "SELECT `uid` FROM `users` WHERE `email`=%s"
+    cursor.execute(sql, (email,))
+    uid = cursor.fetchone()
+    # print(uid, 'aloo1')
+    if uid is None:
+        #print('No such user')
+        return False
+    else:
+        #password = cursor.fetchone()[0]
+        uid = uid[0]
+        # print(uid, 'aloo2')
+        return uid
     connection.close()
-    return password
 
-# def signup(username, password, favorite_color):
-#     connection = pymysql.connect( host = 'localhost', port = 3306,
-#     user = 'alp', passwd = '123456', db = 'todo' )
-#     cursor = connection.cursor()
-#     sql = '''
-#     cursor.execute("""SELECT password FROM users WHERE username = '{username}';""".format(username = username))
-#     exist = cursor.fetchone()
+def getlid(lname):
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql = "SELECT `lid` FROM `lists` WHERE `lname`=%s"
+    cursor.execute(sql, (lname,))
+    lid = cursor.fetchone()
+    if lid is None:
+        #print('No such user')
+        return False
+    else:
+        #password = cursor.fetchone()[0]
+        lid = lid[0]
+        return lid
+    connection.close()
 
-#     if exist is None:
-#         cursor.execute("""INSERT INTO users(username, password, favorite_color)VALUES('{username}', '{password}', '{favorite_color}');""".format(username = username, password=password, favorite_color=favorite_color))
-#         connection.commit()
-#         cursor.close()
-#         connection.close()
+def get_lid_from_tid(tid):
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql = "SELECT `lid` FROM `tasks` WHERE `tid`=%s"
+    cursor.execute(sql, (tid,))
+    lid = cursor.fetchone()
+    if lid is None:
+        #print('No such user')
+        return False
+    else:
+        #password = cursor.fetchone()[0]
+        lid = lid[0]
+        return lid
+    connection.close()
 
-#     else:
-#         return ('User already existed!!!')
+def getlname(lid):
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql = "SELECT `lname` FROM `lists` WHERE `lid`=%s"
+    cursor.execute(sql, (lid,))
+    lname = cursor.fetchone()
+    if lname is None:
+        #print('No such list')
+        return False
+    else:
+        lname = lname[0]
+        return lname
+    connection.close()
 
-#     return 'You have successfully signed up!!!'
+def check_users():
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql = 'SELECT email FROM users ORDER BY uid DESC;'
+    cursor.execute(sql)
+    db_users = cursor.fetchall()
+    users = []
+    for i in range(len(db_users)):
+        person = db_users[i][0]
+        users.append(person)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return users
+
+def check_pw(email):
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql = "SELECT `password` FROM `users` WHERE `email`=%s"
+    cursor.execute(sql, (email,))
+    password = cursor.fetchone()
+    #print(password)
+    if password is None:
+        #print('No such user')
+        return False
+    else:
+        #password = cursor.fetchone()[0]
+        password = password[0]
+        # print(password)
+        return password
+    connection.close()
+
+def signup(email, password):
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql1 = "SELECT `password` FROM `users` WHERE `email`=%s"
+    cursor.execute(sql1, (email,))
+    exist = cursor.fetchone()
+    if exist is None:
+        sql2 = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql2, (email, password))
+        connection.commit()
+        cursor.close
+        connection.close
+        # print('You have succesfully signed up!')
+    else:
+        # print('User already exists!')
+        cursor.close
+        connection.close()
+
+def addlist(uid,lname):
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql1 = "SELECT `uid` FROM `lists` WHERE `lname`=%s"
+    cursor.execute(sql1, (lname,))
+    exist = cursor.fetchone()
+    if exist is None:
+        sql2 = "INSERT INTO `lists` (`uid`, `lname`) VALUES (%s, %s)"
+        cursor.execute(sql2, (uid, lname))
+        connection.commit()
+        cursor.close
+        connection.close
+        # print('List added!')
+    else:
+        # print('List already exists!')
+        cursor.close
+        connection.close()
+
+def list_users_lists(email):
+    uid = getuid(email)
+    #print(uid)
+    list = []
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql = "SELECT `lid`, `lname` FROM `lists` WHERE `uid`=%s"
+    cursor.execute(sql, (uid,))
+    list = cursor.fetchall()
+    print(list)
+    if list is None:
+        # print('No such user')
+        list = []
+        return list
+    else:
+        # print(list)
+        return list
+    connection.close()
+
+def tasklist(lid):
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    # sql = "SELECT `text`, `done` FROM `tasks` WHERE `lid`=%s"
+    sql = "SELECT `tid`, `text`, `done` FROM `tasks` WHERE `lid`=%s"
+    cursor.execute(sql, (lid,))
+    tasklist = []
+    tasklist = cursor.fetchall()
+    #print(tasklist, 'tasklist')
+    if tasklist is None:
+        tasklist = []
+        return tasklist
+        #return False
+    else:
+        # print(tasklist)
+        return tasklist
+    connection.close()
+
+def dellist(lid): 
+    # first delete all tasks belonging to list
+    # lid = getlid(lname)
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql1 = "DELETE  FROM `tasks` WHERE `lid`=%s"
+    cursor.execute(sql1, (lid,))
+    sql2 = "DELETE  FROM `lists` WHERE `lid`=%s"
+    cursor.execute(sql2, (lid,))
+    connection.commit()
+    cursor.close
+    connection.close
+
+def addtask(lid, text):
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql1 = "SELECT `tid` FROM `tasks` WHERE `text`=%s"
+    cursor.execute(sql1, text)
+    #print('ALPAS', text, type(text), 'LAN', text.isspace())
+    exist = cursor.fetchone()
+    if exist is None:
+        sql2 = "INSERT INTO `tasks` (`lid`, `text`, `done`) VALUES (%s, %s, %s)"
+        cursor.execute(sql2, (lid, text, 'N'))
+        connection.commit()
+        cursor.close
+        connection.close
+    else:
+        #print('Task already exists!')
+        cursor.close
+        connection.close()
+
+def deltask(tid):
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql1 = "DELETE  FROM `tasks` WHERE `tid`=%s"
+    cursor.execute(sql1, tid)
+    connection.commit()
+    cursor.close
+    connection.close()
+
+def update_task(tid):
+    connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+    cursor = connection.cursor()
+    sql1 = "SELECT `done` FROM `tasks` WHERE `tid`=%s"
+    cursor.execute(sql1, tid)
+    task_status = cursor.fetchone()[0]
+    print(task_status, 'SOOOON1', tid)
+    if task_status == 'Y':
+        task_status = 'N'
+    else:
+        task_status = 'Y'
+    print(task_status, 'SOOOON2', tid)
+    sql2 = 'UPDATE tasks SET done = %s WHERE tid = %s'
+    cursor.execute(sql2, (task_status, tid))
+    connection.commit()
+    cursor.close
+    connection.close()
--- section3-sessions/todo.py
+++ section3-sessions/todo.py
@@ -1,47 +1,151 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, g
 import model
 
 app = Flask(__name__)
 
+app.secret_key = 'jumpjacks'
+email = ''
+user = model.check_users()
+
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        text1 = 'This web application lets you keep track of your things to do.'
-        text2 = 'In order to use the app you need to be a registered user.'
-        text3 = 'After signing in you can begin using the application.'
+        text1 = 'This web application lets you keep track of your things to do.<br><br>'
+        text2 = 'In order to use the app you need to be a registered user.<br><br>'
+        text3 = 'After signing in you can begin using the application.<br><br>'
         return render_template('index.html', message1=text1, message2=text2, message3=text3)
     else:
-        email = request.form['email']
-        password = request.form['password']
-        db_password = model.check_pw(email)
+        session.pop('email', None)
+        areyouuser = request.form['email']
+        pwd = model.check_pw(areyouuser)
+        if request.form['password'] == pwd:
+            session['email'] = request.form['email']        
+            return redirect(url_for('lists'))
+    if 'email' in session:
+        g.user=session['email']
+        return redirect(url_for('lists'))
+    error_message = 'Wrong email or password <br><br>'
+    return render_template('index.html', err_message = error_message)
 
-        if password == db_password:
-            #message = model.show_color(username)
-            #return render_template('todo.html', message = message)
-            return render_template('todo.html')
-        else:
-            error_message = 'Wrong email or password'
-            return render_template('index.html', err_message = error_message)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('email', None)
+        areyouuser = request.form['email']
+        pwd = model.check_pw(areyouuser)
+        if request.form['password'] == pwd:
+            session['email'] = request.form['email']
+            return redirect(url_for('lists'))
+    return render_template('index.html')
+
+@app.before_request
+def before_request():
+    g.email = None
+    if 'email' in session:
+        g.email = session['email']
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
 
 @app.route('/about', methods = ['GET'])
 def about():
-    return render_template('about.html')
+    return render_template('about.html', message = 'We are supersonic')
 
 @app.route('/terms', methods = ['GET'])
 def terms():
-    return render_template('terms.html')
+    return render_template('terms.html', message = 'He are our terms of use')
 
 @app.route('/privacy', methods = ['GET'])
 def privacy():
-    return render_template('privacy.html')
+    return render_template('privacy.html', message = 'Very private')
 
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    if request.method == 'GET':
+        return render_template('signup.html', message1 = 'Please sign up')
+    else:
+        email = request.form["email"]
+        password = request.form["password"]
+        model.signup(email, password)
+        #message = model.signup(email, password)
+        return render_template('signup.html', message2 = 'Thanks for signing up. <a href="/login">Click to log in</a>')
 
-@app.route('/todo', methods = ['GET'])
-def todo():
-    return render_template('todo.html')
+@app.route('/lists')
+def lists():
+    email = session['email']
+    lists = model.list_users_lists(email)
+    print(lists, 'KAPLAN')
+    return render_template('new_lists.html', lists=lists, message = email)
+
+@app.route("/dellist/<string:lid>")
+def dellist(lid):
+    #lname = request.args.get('list')
+    # print(lname,'ALPASLAN lname')
+    model.dellist(lid)
+    #print(lname,'sdfsd fsdf aALOOOOOOOOOOOO **0*0*0*00999 ')
+    # db.session.delete(todo)
+    # db.session.commit()
+    return redirect(url_for("lists"))
+
+@app.route('/tasks')
+def tasks():
+    # dene1 = request.data
+    lname = request.args.get('list')
+    #print(lname, 'lname')
+    lid = model.getlid(lname)
+    # print(lname, lid, 'lid')
+    tasks = model.tasklist(lid)
+    #print(tasks, 'tasks - alp')
+    # return render_template('tasks.html', tasks=tasks)
+    return render_template('new_tasks.html', tasks=tasks, lid=lid)
+
+@app.route('/addlist', methods = ['POST'])
+def addlist():
+    email = session['email']
+    uid = model.getuid(email)
+    lname = request.form["lname"]
+    model.addlist(uid,lname)
+    #return render_template('lists.html', message='List added')
+    return redirect(url_for('lists'))
+    
+@app.route('/addtask', methods = ['POST'])
+def addtask():
+    #task = request.form["lname_degil_baska_bisi_tabi"]
+    task = request.form.get("text")
+    lid  = request.args.get('list')
+    lname = model.getlname(lid)
+    # print(task, lid, lname, 'Alp ***')
+    model.addtask(lid, task)
+    # return redirect(url_for('/tasks'))
+    myurl = '/tasks?list=' + lname
+    #print(myurl)
+    return redirect(myurl)
+
+@app.route("/deltask/<string:tid>")
+def deltask(tid):
+    lid = model.get_lid_from_tid(tid)
+    lname = model.getlname(lid)
+    #lname = request.args.get('list')
+    model.deltask(tid)
+    #print(lname,'sdfsd fsdf aALOOOOOOOOOOOO **0*0*0*00999 ')
+    # db.session.delete(todo)
+    # db.session.commit()
+    # return redirect(url_for("tasks"))
+    myurl = '/tasks?list=' + lname
+    #print(myurl)
+    return redirect(myurl)
+
+@app.route("/updatetask/<string:tid>")
+def updatetask(tid):
+    lid = model.get_lid_from_tid(tid)
+    lname = model.getlname(lid)
+    model.update_task(tid)
+    #todo.complete = not todo.complete
+    myurl = '/tasks?list=' + lname
+    #print(myurl)
+    return redirect(myurl)
 
 if __name__ == '__main__':
     app.run(debug=True)
