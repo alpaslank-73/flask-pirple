f3318c47a49622866015d12ccc89bb34ae58b513
alpaslank-73 <alp@perception.com.tr>
2020-11-03 20:11:55 +0300

    todo bootstrap final

---
+++ section3-todo-admin/seed.py
@@ -0,0 +1,18 @@
+import pymysql 
+
+connection = pymysql.connect( host = 'localhost', port = 3306,
+    user = 'alp', passwd = '123456', db = 'todo' )
+cursor = connection.cursor()
+
+# sql1 = """INSERT INTO users( email, password) VALUES( 'alp', '123');"""
+# cursor.execute(sql1)
+
+sql2 = """INSERT INTO lists( uid, lname) VALUES( '38', 'physics');"""
+cursor.execute(sql2)
+
+sql3 = """INSERT INTO tasks( lid, text, done) VALUES( '1', 'algebra', 'Y');"""
+cursor.execute(sql3)
+
+connection.commit()
+cursor.close()
+connection.close()
